5fae62274d51d3b904819772bc0aa3a54ce5f772
Jin Sung Lee <jinsunglee@truesage.local>
2020-05-21 16:58:10 +0900

    branch 1 commit 2

--- AppSearcher/AppDelegate.swift
+++ AppSearcher/AppDelegate.swift
@@ -22,6 +22,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func branch1() {
+        // commit 2
+    }
 
     func branch2() {
         // commit 1
